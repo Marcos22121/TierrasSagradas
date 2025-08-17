6a31071baf797a93caccdf9434be8020c36c7561
marcos <75872914+Marcos22121@users.noreply.github.com>
2025-08-16 21:52:20 -0300

    carpetas organizadas + gif

---
+++ Includes/header.php
@@ -0,0 +1,19 @@
+<head>
+    <link rel="stylesheet" href="../Style/header.css">
+</head>
+
+<header>
+    <div>
+        <img src="/C:/xampp/htdocs/TierrasSagradasRework/assets/logo.png" alt="Logo" height="60">
+        <h1>Tierras Sagradas</h1>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="/C:/xampp/htdocs/TierrasSagradasRework/index.php">Inicio</a></li>
+            <li><a href="/C:/xampp/htdocs/TierrasSagradasRework/que-es.php">¿Qué es Tierras Sagradas?</a></li>
+            <li><a href="/C:/xampp/htdocs/TierrasSagradasRework/historias.php">Historias</a></li>
+            <li><a href="/C:/xampp/htdocs/TierrasSagradasRework/personajes.php">Personajes</a></li>
+            <li><a href="/C:/xampp/htdocs/TierrasSagradasRework/tierras.php">Tierras</a></li>
+        </ul>
+    </nav>
+</header>
---
+++ Indices/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tierras Sagradas</title>
+    <link rel="stylesheet" href="../Style/index.css">
+</head>
+<body>
+    <?php include '../Includes/header.php'; ?>
+
+    <!-- HERO CARD -->
+   <section class="hero-card">
+    <div class="hero-gif">
+        <img src="../img/Indices/ezgif.gif" alt="Hero GIF">
+    </div>
+</section>
+
+
+    <?php include '../Includes/footer.php'; ?>
+</body>
+</html>
