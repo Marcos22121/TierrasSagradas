802bb14ebdac6c5aad0a4cda0ded0396f13edde2
marcos <75872914+Marcos22121@users.noreply.github.com>
2025-08-25 01:09:34 -0300

    la puta q me pario

--- Indices/index.php
+++ Indices/index.php
@@ -69,60 +69,10 @@
 
 <div class="content-wrapper"> 
 
-    <section id="dioses">
-        <h2>Personajes Destacados</h2>
 
-<section class="cards-container">
+
     
-<div class="card-skewed" aria-label="Dios Nerok">
-    <div class="particles-container" id="particles-nerok"></div>
-    
-    <!-- Contenedor de imágenes -->
-    <div class="card-img-wrapper">
-        <!-- Gif de galaxia (fondo base) -->
-        <img src="../img/Indices/gg.gif" alt="Nerok fondo" class="card-bg">
-    </div>
 
-    <!-- Imagen transparente (efecto en hover) -->
-    <img src="../img/Indices/overlay.png" alt="Nerok overlay" class="card-overlay">
-
-    <!-- Contenido con gradiente de fondo -->
-    <div class="card-skewed-content">
-        <h3>Binohmo</h3>
-        <p>Dios y Creador supremo</p>
-    </div>
-</div>
-
-<div class="card-skewed" aria-label="Tobias">
-    <div class="particles-container" id="particles-tobias"></div>
-    <!-- Contenedor de imágenes -->
-    <div class="card-img-wrapper">
-        <!-- Fondo base -->
-        <img src="../img/Indices/bgShelby.png" alt="Tobias fondo" class="card-bg">
-    </div>
-    <!-- Imagen transparente (efecto en hover) -->
-    <img src="../img/Indices/shelby.png" alt="Tobias overlay" class="card-overlay">
-    <!-- Contenido con gradiente de fondo -->
-    <div class="card-skewed-content">
-        <h3>Tobi Shelby</h3>
-        <p>El ingeniero, inventor y visionario de las Tierras Sagradas</p>
-    </div>
-</div>
-
-<div class="card-skewed" aria-label="Nuevo Personaje">
-    <div class="particles-container" id="particles-nuevo"></div>
-    <div class="card-img-wrapper">
-        <!-- Cambia la imagen de fondo aquí -->
-        <img src="../img/Indices/aman.png" alt="Nuevo fondo" class="card-bg">
-    </div>
-    <!-- Cambia la imagen overlay aquí -->
-    <img src="../img/Indices/augrim.png" alt="Nuevo overlay" class="card-overlay">
-    <div class="card-skewed-content">
-        <h3>Augrim</h3>
-        <p>Héroe Legendario de las Montañas Luminosas</p>
-    </div>
-</div>
-    
 </section>
 <div id="map-section" class="mapa-h2">
 <h2>Mapa Interactivo</h2>
@@ -141,7 +91,7 @@
 
 
 
-    </div>
+</div>
 
     
 
